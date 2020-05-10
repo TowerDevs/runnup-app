0e085df7539af802c8490409b3c6c490b7966195
Richard Antao <richardmantao@gmail.com>
2020-05-10 18:44:40 -0400

    Created auth state branch

---
+++ src/reducers/auth.js
@@ -0,0 +1,58 @@
+import {
+    AUTH_ERROR,
+    USER_REQUESTED, USER_LOADED,
+    REGISTER_SUCCESS, REGISTER_FAILED,
+    LOGIN_SUCCESS, LOGIN_FAILED,
+    LOGOUT_SUCCESS
+} from "../actions/types";
+
+const initialState = {
+    // token: localStorage.getItem("token"), substitute for auth method being used
+    isAuthenticated: null,
+    isLoading: false,
+    user: null
+};
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case USER_REQUESTED:
+            return {
+                ...state,
+                isLoading: true
+            };
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                isLoading: false,
+                user: action.payload
+            };
+        case REGISTER_SUCCESS:
+            return {
+                ...state,
+                ...action.payload,
+                isLoading: false
+            };
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                ...action.payload,
+                token: action.payload.token, // verify payload from backend
+                isAuthenticated: true,
+                isLoading: false
+            };
+        case AUTH_ERROR:
+        case REGISTER_FAILED:
+        case LOGIN_FAILED:
+        case LOGOUT_SUCCESS:
+            return {
+                ...state,
+                token: null,
+                user: null,
+                isAuthenticated: false,
+                isLoading: false
+            };
+        default:
+            return state;
+    };
+};
