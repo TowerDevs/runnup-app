cfdd7ec491aff19d18db7c85f0e76917b8fd06eb
Richard Antao <richardmantao@gmail.com>
2020-05-10 01:35:45 -0400

    PREVIEW: Proposing App as class for logging

--- src/App.js
+++ src/App.js
@@ -3,18 +3,81 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SplashScreen } from "expo";
 import * as Font from "expo-font";
-import * as React from "react";
+import React, { Component } from "react";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
 
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
 import LinkingConfiguration from "./navigation/LinkingConfiguration";
 
 import { Provider } from "react-redux";
+import { connect } from "react-redux";
+import { logErrors } from "./actions/errors";
+import PropTypes from "prop-types";
 import store from "./store";
 
 // TODO: Import firebase once config is tested
 
 const Stack = createStackNavigator();
+
+class App extends Component {
+  state = {
+    isLoadingComplete: false
+  };
+
+  static propTypes = {
+    logErrors: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    const { logErrors } = this.props;
+
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHide();
+
+        // Load fonts
+        await Font.loadAsync({
+          ...Ionicons.font,
+          "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+        logErrors(e); // send e to backend logger
+      } finally {
+        this.setState({ isLoadingComplete: true });
+        SplashScreen.hide();
+      }
+    };
+
+    loadResourcesAndDataAsync();
+  };
+
+  render() {
+    const { isLoadingComplete } = this.state;
+    const { skipLoadingScreen } = this.props;
+
+    if (!isLoadingComplete && !skipLoadingScreen) return null;
+
+    return (
+      <Provider store={store}>
+        <View style={styles.container}>
+          {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
+          <NavigationContainer linking={LinkingConfiguration}>
+            <Stack.Navigator>
+              <Stack.Screen name="Root" component={BottomTabNavigator} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </View>
+      </Provider>
+    );
+  }
+};
+
+export default connect(null, { logErrors })(App);
+
+/* Class above with redux-connect, function below */
+/* Could alternatively wrap App UI in an ErrorBoundary component and log the errors in that component, preserving App */
+/* ^ This allows to keep App a function and the top-component clean */
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
