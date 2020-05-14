4de6c34870c07300b420ce3f7fa3efce811aefd1
Richard Antao <richardmantao@gmail.com>
2020-05-14 12:15:13 -0400

    Initialized HamburgerMenu

---
+++ src/components/HamburgerMenu.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+// import PropTypes from "prop-types";
+
+const HamburgerMenu = () => {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+};
+
+HamburgerMenu.propTypes = {
+
+};
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+});
+
+export default HamburgerMenu;
