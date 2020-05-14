abb16f91ae8b70736d28dd2def4bd71337cfe898
Jason Chin <jason.rob.chin@gmail.com>
2020-05-14 15:13:49 -0400

    add global styles

---
+++ src/constants/Styles.js
@@ -0,0 +1,13 @@
+export default {
+  button: {
+    borderRadius: 30,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+}
