8528df3075c2af9044a7d9278dd79923aef3789f
Richard Antao <richardmantao@gmail.com>
2020-05-13 21:08:54 -0400

    Added SecureStore to handle token storage

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -8,9 +8,10 @@ import {
     EMAIL_TOKEN_SENT, EMAIL_TOKEN_ERROR, EMAIL_VERIFIED,
     PASSWORD_TOKEN_SENT, PASSWORD_TOKEN_ERROR, PASSWORD_TOKEN_VERIFIED
 } from "../actions/types";
+import * as Storage from "expo-secure-store";
 
 const initialState = {
-    // token: localStorage.getItem("token"), substitute for auth method being used
+    token: Storage.getItemAsync("accessToken"),
     isAuthenticated: null,
     isLoading: false,
     user: null
@@ -27,39 +28,37 @@ export default (state = initialState, action) => {
         case EMAIL_VERIFIED:
         case PASSWORD_TOKEN_SENT:
         case PASSWORD_TOKEN_VERIFIED:
-            return {
-                ...state,
-                isLoading: false
-            };
+        return {
+            ...state,
+            isLoading: false
+        };
         case USER_LOADED:
             return {
                 ...state,
-                isAuthenticated: true,
                 isLoading: false,
                 user: action.payload
             };
         case REGISTER_SUCCESS:
-            return {
-                ...state,
-                ...action.payload,
-                isLoading: false
-            };
         case LOGIN_SUCCESS:
+            Storage.setItemAsync("accessToken", action.payload);
             return {
                 ...state,
                 ...action.payload,
-                token: action.payload.token, // verify payload from backend
                 isAuthenticated: true,
                 isLoading: false
             };
+        case DEREGISTER_SUCCESS:
+            return {
+
+            };
         case AUTH_ERROR:
         case REGISTER_FAILED:
+        case DEREGISTER_FAILED:
         case LOGIN_FAILED:
         case LOGOUT_SUCCESS:
-        case DEREGISTER_SUCCESS:
-        case DEREGISTER_FAILED:
         case EMAIL_TOKEN_ERROR:
         case PASSWORD_TOKEN_ERROR:
+            Storage.deleteItemAsync("accessToken");
             return {
                 ...state,
                 token: null,
