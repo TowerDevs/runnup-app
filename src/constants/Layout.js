6f9f9a32739f54d38f37bd97889d277e0effcef6
Richard Antao <richardmantao@gmail.com>
2020-05-14 20:33:46 -0400

    Added properties for layouts

--- src/constants/Layout.js
+++ src/constants/Layout.js
@@ -4,6 +4,51 @@ const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
 export default {
+  align: {
+    start: {
+      alignItems: "flex-start"
+    },
+    center: {
+      alignItems: "center"
+    },
+    end: {
+      alignItems: "flex-end"
+    },
+    stretch: {
+      alignItems: "stretch"
+    }
+  },
+  direction: {
+    row: {
+      flexDirection: "row"
+    },
+    rowRev: {
+      flexDirection: "row-reverse"
+    },
+    col: {
+      flexDirection: "row"
+    },
+    colRev: {
+      flexDirection: "row-reverse"
+    }
+  },
+  justifyContent: {
+    start: {
+      justifyContent: "flex-start"
+    },
+    center: {
+      justifyContent: "center"
+    },
+    end: {
+      justifyContent: "flex-end"
+    },
+    between: {
+      justifyContent: "space-between"
+    },
+    around: {
+      justifyContent: "space-around"
+    }
+  },
   window: {
     width,
     height,
