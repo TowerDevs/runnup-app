7f8ef54e49a882ac7bb8a435341d73cab0ccacfe
Richard Antao <richardmantao@gmail.com>
2020-05-24 16:15:19 -0400

    Moved tokenConfig to utils

---
+++ src/utils/tokenConfig.js
@@ -0,0 +1,18 @@
+/**
+ * @desc Configure the authorization token for private requests
+ * @param {function} getState - function to retrieve the token from the auth reducer
+ * @returns {Object} - the configuration properties
+ */
+export default getState => {
+    const token = getState().auth.token;
+
+    const config = {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    if (token) config.headers["Authorization"] = `Basic ${token}`;
+
+    return config;
+};
