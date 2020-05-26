155b3e5bddb6eba90c06e67fe06660324f99350e
Richard Antao <richardmantao@gmail.com>
2020-05-26 12:19:27 -0400

    Defined Config interface

--- src/utils/tokenConfig.ts
+++ src/utils/tokenConfig.ts
@@ -1,9 +1,13 @@
+export interface Config {
+    header: object;
+};
+
 /**
  * @desc Configure the authorization token for private requests
  * @param {function} getState - function to retrieve the token from the auth reducer
  * @returns {Object} - the configuration properties
  */
-export default getState => {
+export default (getState: Function) => {
     const token = getState().auth.token;
 
     const config = {
