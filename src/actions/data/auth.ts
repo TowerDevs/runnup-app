238cc8da653c12fa519b80f9bb8a09d1d0765582
Richard Antao <richardmantao@gmail.com>
2020-05-26 14:38:15 -0400

    Added types to action creators

--- src/actions/data/auth.ts
+++ src/actions/data/auth.ts
@@ -5,18 +5,19 @@ import {
     DEREGISTER_SUCCESS, DEREGISTER_FAILED,
     LOGIN_SUCCESS, LOGIN_FAILED,
     LOGOUT_SUCCESS,
-    EMAIL_TOKEN_SENT, EMAIL_VERIFIED,
-    PASSWORD_TOKEN_SENT, PASSWORD_TOKEN_VERIFIED
+    EMAIL_TOKEN_SENT, EMAIL_TOKEN_ERROR, EMAIL_VERIFIED,
+    PASSWORD_TOKEN_SENT, PASSWORD_TOKEN_ERROR, PASSWORD_TOKEN_VERIFIED,
+    RegisterReq, LoginReq, TokenRes, UserRes, AuthActions
 } from "../../types/Auth";
-import tokenConfig from "../../utils/tokenConfig";
+import tokenConfig, { Config } from "../../utils/tokenConfig";
 import { returnErrors } from "../errors";
-import axios from "axios";
+import axios, { AxiosResponse, AxiosError } from "axios";
 
 /**
  * @desc Set the Auth branch to isLoading
  * @returns {Object} - loading async action type
  */
-const setLoading = () => {
+const setLoading = ():AuthActions => {
     return {
         type: USER_REQUESTED
     };
@@ -28,28 +29,30 @@ const setLoading = () => {
  * @param {function} dispatch - function to dispatch the action
  * @returns {Object} - the new User and the access token for automatic login upon registration
  */
-export const registerUser = user => dispatch => {
-    const config = {
+export const registerUser = (user: RegisterReq) => (
+    dispatch: Function
+):void => {
+    const config: Config = {
         headers: {
             "Content-Type": "application/json"
         }
     };
 
-    axios.post("/api/v1/users", user, config)
-    .then(res => dispatch({
+    axios.post<TokenRes>("/api/v1/users", user, config)
+    .then((res: AxiosResponse<TokenRes>) => dispatch({
         type: REGISTER_SUCCESS,
         payload: res.data
     }))
-    .catch(err => {
+    .catch((err: AxiosError) => {
         if(err.response) {
-            dispatch(returnErrors(err.response.data, err.response.status, "REGISTER_FAILED"))
+            dispatch(returnErrors(err.response.data, err.response.status, REGISTER_FAILED))
             dispatch({ type: AUTH_ERROR });
         } else if(err.request) {
-            dispatch(returnErrors(err.request.data, err.request.status, "REGISTER_FAILED"));
+            dispatch(returnErrors(err.request.data, err.request.status, REGISTER_FAILED));
             dispatch({ type: AUTH_ERROR });
         }
 
-        dispatch(returnErrors("An internal error occurred", 500, "REGISTER_FAILED"));
+        dispatch(returnErrors("An internal error occurred", 500, REGISTER_FAILED));
         dispatch({ type: AUTH_ERROR });
     });
 };
@@ -59,43 +62,48 @@ export const registerUser = user => dispatch => {
  * @param {function} dispatch - function for dispatching the action
  * @returns {Object} - returns the user's _id, name, email, etc.
  */
-export const deleteUser = () => dispatch => {
+export const deleteUser = () => (
+    dispatch: Function
+):void => {
     axios.delete("/api/v1/users")
     .then(() => dispatch({
         type: DEREGISTER_SUCCESS
     }))
-    .catch(err => {
+    .catch((err: AxiosError) => {
         if(err.response) {
-            dispatch(returnErrors(err.response.data, err.response.status, "DEREGISTER_FAILED"))
+            dispatch(returnErrors(err.response.data, err.response.status, DEREGISTER_FAILED))
             dispatch({ type: AUTH_ERROR });
         } else if(err.request) {
-            dispatch(returnErrors(err.request.data, err.request.status, "DEREGISTER_FAILED"));
+            dispatch(returnErrors(err.request.data, err.request.status, DEREGISTER_FAILED));
             dispatch({ type: AUTH_ERROR });
         }
 
-        dispatch(returnErrors("An internal error occurred", 500, "DEREGISTER_FAILED"));
+        dispatch(returnErrors("An internal error occurred", 500, DEREGISTER_FAILED));
         dispatch({ type: AUTH_ERROR });
     });
 };
 
-export const loadUser = () => (dispatch, getState) => {
+export const loadUser = () => (
+    dispatch: Function,
+    getState: Function
+):void => {
     dispatch(setLoading());
 
-    axios.get("/api/v1/users", tokenConfig(getState))
-    .then(res => dispatch({
+    axios.get<UserRes>("/api/v1/users", tokenConfig(getState))
+    .then((res: AxiosResponse<UserRes>) => dispatch({
         type: USER_LOADED,
         payload: res.data
     }))
-    .catch(err => {
+    .catch((err: AxiosError) => {
         if(err.response) {
-            dispatch(returnErrors(err.response.data, err.response.status, "AUTH_ERROR"))
+            dispatch(returnErrors(err.response.data, err.response.status, AUTH_ERROR))
             dispatch({ type: AUTH_ERROR });
         } else if(err.request) {
-            dispatch(returnErrors(err.request.data, err.request.status, "AUTH_ERROR"));
+            dispatch(returnErrors(err.request.data, err.request.status, AUTH_ERROR));
             dispatch({ type: AUTH_ERROR });
         }
 
-        dispatch(returnErrors("An internal error occurred", 500, "AUTH_ERROR"));
+        dispatch(returnErrors("An internal error occurred", 500, AUTH_ERROR));
         dispatch({ type: AUTH_ERROR });
     });
 };
@@ -106,28 +114,30 @@ export const loadUser = () => (dispatch, getState) => {
  * @param {function} dispatch - function to dispatch the action
  * @returns {Object} - contains the newly created access token for the login
  */
-export const loginUser = credentials => dispatch => {
-    const config = {
+export const loginUser = (credentials: LoginReq) => (
+    dispatch: Function
+):void => {
+    const config: Config = {
         headers: {
             "Content-Type": "application/json"
         }
     };
 
-    axios.post("/api/v1/users/access-token", credentials, config)
-    .then(res => dispatch({
+    axios.post<TokenRes>("/api/v1/users/access-token", credentials, config)
+    .then((res: AxiosResponse<TokenRes>) => dispatch({
         type: LOGIN_SUCCESS,
         payload: res.data
     }))
-    .catch(err => {
+    .catch((err: AxiosError) => {
         if(err.response) {
-            dispatch(returnErrors(err.response.data, err.response.status, "LOGIN_FAILED"))
+            dispatch(returnErrors(err.response.data, err.response.status, LOGIN_FAILED))
             dispatch({ type: AUTH_ERROR });
         } else if(err.request) {
-            dispatch(returnErrors(err.request.data, err.request.status, "LOGIN_FAILED"));
+            dispatch(returnErrors(err.request.data, err.request.status, LOGIN_FAILED));
             dispatch({ type: AUTH_ERROR });
         }
 
-        dispatch(returnErrors("An internal error occurred", 500, "LOGIN_FAILED"));
+        dispatch(returnErrors("An internal error occurred", 500, LOGIN_FAILED));
         dispatch({ type: AUTH_ERROR });
     });
 };
@@ -137,7 +147,9 @@ export const loginUser = credentials => dispatch => {
  * @param {function} dispatch - function to dispatch the action Object
  * @returns {Object} - contains the action type
  */
-export const logoutUser = () => dispatch => {
+export const logoutUser = () => (
+    dispatch: Function
+):void => {
     axios.delete("/api/v1/users/access-token")
     .then(() => dispatch({
         type: LOGOUT_SUCCESS
@@ -152,17 +164,19 @@ export const logoutUser = () => dispatch => {
  * @param {function} dispatch - function to dispatch the action Object
  * @returns {Object} - contains the action type
  */
-export const sendEmailToken = () => dispatch => {
+export const sendEmailToken = () => (
+    dispatch: Function
+):void => {
     axios.get("/v1/users/email/token")
     .then(() => dispatch({
         type: EMAIL_TOKEN_SENT
     }))
-    .catch(err => {
-        if(err.response) dispatch(returnErrors(err.response.data, err.response.status, "EMAIL_TOKEN_ERROR"));
+    .catch((err: AxiosError) => {
+        if(err.response) dispatch(returnErrors(err.response.data, err.response.status, EMAIL_TOKEN_ERROR));
 
-        else if(err.request) dispatch(returnErrors(err.request.data, err.request.status, "EMAIL_TOKEN_ERROR"));
+        else if(err.request) dispatch(returnErrors(err.request.data, err.request.status, EMAIL_TOKEN_ERROR));
 
-        dispatch(returnErrors("An internal error occurred", 500, "EMAIL_TOKEN_ERROR"));
+        dispatch(returnErrors("An internal error occurred", 500, EMAIL_TOKEN_ERROR));
     });
 };
 
@@ -171,17 +185,19 @@ export const sendEmailToken = () => dispatch => {
  * @param {function} dispatch - function to dispatch the action Object
  * @returns {Object} - contains the action type
  */
-export const verifyEmail = token => dispatch => {
+export const verifyEmail = (token: string) => (
+    dispatch: Function
+):void => {
     axios.get(`/v1/users/email/token/${token}`)
     .then(() => dispatch({
         type: EMAIL_VERIFIED
     }))
-    .catch(err => {
-        if(err.response) dispatch(returnErrors(err.response.data, err.response.status, "EMAIL_TOKEN_ERROR"));
+    .catch((err: AxiosError) => {
+        if(err.response) dispatch(returnErrors(err.response.data, err.response.status, EMAIL_TOKEN_ERROR));
 
-        else if(err.request) dispatch(returnErrors(err.request.data, err.request.status, "EMAIL_TOKEN_ERROR"));
+        else if(err.request) dispatch(returnErrors(err.request.data, err.request.status, EMAIL_TOKEN_ERROR));
 
-        dispatch(returnErrors("An internal error occurred", 500, "EMAIL_TOKEN_ERROR"));
+        dispatch(returnErrors("An internal error occurred", 500, EMAIL_TOKEN_ERROR));
     });
 };
 
@@ -190,17 +206,19 @@ export const verifyEmail = token => dispatch => {
  * @param {function} dispatch - function to dispatch the action Object
  * @returns {Object} - contains the action type
  */
-export const requestNewPassword = () => dispatch => {
+export const requestNewPassword = () => (
+    dispatch: Function
+):void => {
     axios.get("/v1/users/password")
     .then(() => dispatch({
         type: PASSWORD_TOKEN_SENT
     }))
-    .catch(err => {
-        if(err.response) dispatch(returnErrors(err.response.data, err.response.status, "PASSWORD_TOKEN_ERROR"));
+    .catch((err: AxiosError) => {
+        if(err.response) dispatch(returnErrors(err.response.data, err.response.status, PASSWORD_TOKEN_ERROR));
 
-        else if(err.request) dispatch(returnErrors(err.request.data, err.request.status,"PASSWORD_TOKEN_ERROR"));
+        else if(err.request) dispatch(returnErrors(err.request.data, err.request.status,PASSWORD_TOKEN_ERROR));
 
-        dispatch(returnErrors("An internal error occurred", 500, "PASSWORD_TOKEN_ERROR"));
+        dispatch(returnErrors("An internal error occurred", 500, PASSWORD_TOKEN_ERROR));
     });
 };
 
@@ -209,16 +227,18 @@ export const requestNewPassword = () => dispatch => {
  * @param {function} dispatch - function to dispatch the action Object
  * @returns {Object} - contains the action type
  */
-export const verifyPasswordToken = token => dispatch => {
+export const verifyPasswordToken = (token: string) => (
+    dispatch: Function
+):void => {
     axios.get(`/v1/users/password/${token}`)
     .then(() => dispatch({
         type: PASSWORD_TOKEN_VERIFIED
     }))
-    .catch(err => {
-        if(err.response) dispatch(returnErrors(err.response.data, err.response.status, "PASSWORD_TOKEN_ERROR"));
+    .catch((err: AxiosError) => {
+        if(err.response) dispatch(returnErrors(err.response.data, err.response.status, PASSWORD_TOKEN_ERROR));
 
-        else if(err.request) dispatch(returnErrors(err.request.data, err.request.status, "PASSWORD_TOKEN_ERROR"));
+        else if(err.request) dispatch(returnErrors(err.request.data, err.request.status, PASSWORD_TOKEN_ERROR));
 
-        dispatch(returnErrors("An internal error occurred", 500, "PASSWORD_TOKEN_ERROR"));
+        dispatch(returnErrors("An internal error occurred", 500, PASSWORD_TOKEN_ERROR));
     });
 };
